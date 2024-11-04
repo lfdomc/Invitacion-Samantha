b9a9d58c51c30820f7ea80ad815c524b7153f2d8
Luis Morua <lfdomc@gmail.com>
2024-11-04 07:53:36 -0600

    cuatro30

--- src/components/ButtonMaps/Button.tsx
+++ src/components/ButtonMaps/Button.tsx
@@ -1,7 +1,7 @@
 import './Button.css';
 const ButtonComponent: React.FC = () => {
   const handleClick = () => {
-    window.open("https://www.google.com/maps", "_blank");
+    window.open("https://www.google.com/maps?gs_lcrp=EgZjaHJvbWUqEAgCEC4YChgWGB4YrwEYxwEyBggAEEUYOTIGCAEQRRhAMhAIAhAuGAoYFhgeGK8BGMcBMgoIAxAAGIAEGKIEMgoIBBAAGIAEGKIEMgoIBRAAGIAEGKIEMgoIBhAAGIAEGKIEMgYIBxBFGEDSAQgzOTU4ajBqN6gCCLACAQ&um=1&ie=UTF-8&fb=1&gl=cr&sa=X&geocode=KQvXUTSw8aCPMaaUvzCEN1g7&daddr=114+Heredia,+Heredia,+Barva", "_blank");
   };
 
   return (
