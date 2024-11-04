a84727b99c8a52227582250177485c5f55875eeb
Luis Morua <lfdomc@gmail.com>
2024-11-04 07:50:22 -0600

    cuatro29

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -14,12 +14,14 @@ export default function Background() {
       <img src={mulanImage} className='mulan22' alt="mulan" />
       <section className='cuadro1'>
       <p className='parrafo1'>Los invitamos a celebrar el cumpleaños de:</p>
+      <p className='fecha'>7 Años</p>
       <h1 className='title'>Samantha Morúa Madríz</h1>
       <p className='fecha'>08|12|2024</p>
-      <p className='parrafo'>De 1:pm a 6:30 pm</p>
+      <p className='parrafo'>De 1:pm a 7:30 pm</p>
       <p className='parrafo'> Dirección: V Entertaiment</p>
       <p className='parrafotext'> 1 km norte de la iglesia Católica de San José de la 
         Montaña. Heredia Costa Rica</p>
+        <p className='parrafotext'> Recuerden traer Abrigo!!</p>
       </section>
 
       </section>
