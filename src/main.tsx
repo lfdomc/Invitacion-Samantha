ecdc59ac2587c4f66900700c7ca58e59a5610d9d
Luis Morua <lfdomc@gmail.com>
2024-11-04 02:44:25 -0600

    cuatro25

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './reset.css'
-import { Header } from './components/Header/Header.tsx'
 import Background from './components/Background/Background.tsx'
-
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header/>
+    
     <Background/>
   </StrictMode>,
 )
