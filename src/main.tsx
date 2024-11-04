2053bf7f951924741e2c4153c58ad20bba262685
Luis Morua <lfdomc@gmail.com>
2024-11-03 21:31:04 -0600

    cuatro5

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './reset.css'
 import { Header } from './components/Header/Header.tsx'
 import Background from './components/Background/Background.tsx'
-import Bloque1 from './components/Bloque1/Bloque1.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
